a98f900d54003a74ed6e840d27125f9a5909b6f9
martinapiglio <martina.piglio@gmail.com>
2023-03-03 16:19:46 +0100

    js script: square added (100) + styling corr

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('test');
+let containerElement = document.getElementById('container');
+let cellNumber = 100;
+let cellPerRow = Math.sqrt(cellNumber);
+
+console.log(cellPerRow);
+
+for (i = 0; i < cellNumber; i++) {
+
+    let newSquare = document.createElement('div');
+    containerElement.append(newSquare);
+    newSquare.classList.add("square");
+    newSquare.style.width= "calc(100% / " + cellPerRow + " )";
+    newSquare.innerText = i + 1;
+
+}
